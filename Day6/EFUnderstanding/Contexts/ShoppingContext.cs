cf18c2b9f567fea2f567359e4fbbd33f1c84032b
unknown <gayat_19@yahoo.com>
2024-11-06 12:51:12 +0530

    Day6

---
+++ Day6/EFUnderstanding/Contexts/ShoppingContext.cs
@@ -0,0 +1,19 @@
+﻿using EFUnderstanding.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFUnderstanding.Contexts
+{
+    public class ShoppingContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-KVHT2L3\SQLEXPRESS;Database=dbShopping;TrustServerCertificate=True;Integrated security=true");
+        }
+        public DbSet<Customer> Customers { get; set; } //By default this property name will be the table name
+    }
+}
---
+++ Day6/EFUnderstanding/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFUnderstanding.Models
+{
+    public class Customer : IEquatable<Customer>, IComparable<Customer>
+    {
+        //since its int -> identity will be included automatically
+        public int Id { get; set; }//Id indicates that this will be teh primary key of the table
+        public string Name { get; set; } = string.Empty;
+        public decimal Phone { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+
+        public int CompareTo(Customer? other)
+        {
+            var customer = other ?? new Customer();
+            return customer.Name.CompareTo(this.Name);
+        }
+
+        public bool Equals(Customer? other)
+        {
+            var customer = other ?? new Customer();
+            return customer.Id.Equals(this.Id);
+        }
+        public override string ToString()
+        {
+            return $"Id {Id}\tName {Name}\tPhone {Phone}\tEmail {Email}";
+        }
+    }
+
+}
